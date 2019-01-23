e4a704c9356f12d62500e9059d7ea7fbcf77d11f
yshh160 <lei.xiayang@belle.com.cn>
2019-01-23 17:49:03 +0800

    修改配置

--- apitest/settings.py
+++ apitest/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'interface_platform',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'mysql123456',
         'HOST':'localhost',
         'PORT':'3306',
     }
